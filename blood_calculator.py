7f7598391eab01976a792b0f25454e7614d47f81
Nikhita <nikhita.gopisetty@duke.edu>
2023-01-20 15:55:43 -0500

    Added LDL_analysis function

--- blood_calculator.py
+++ blood_calculator.py
@@ -40,4 +40,15 @@ def LDL_input():
   LDL_value = int(LDL_value)
   return LDL_value
 
+def LDL_analysis(LDL_int):
+  if LDL_int < 130:
+    answer = "Normal"
+  elif 130 <= LDL_int < 160:
+    answer = "Borderline High"
+  elif 160 <= LDL_int < 190:
+    answer = "High"
+  else:
+    answer = "Very High"
+  return answer
+
 interface()
