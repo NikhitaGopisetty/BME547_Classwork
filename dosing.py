5a758bf3b45886c0f0487ff82e6107afaddddac2
Nikhita <nikhita.gopisetty@duke.edu>
2023-01-27 12:41:54 -0500

    Refactored dosing.py, making new functions after Dr. Ward spoke to us

--- dosing.py
+++ dosing.py
@@ -7,10 +7,12 @@ pediatric
 """
 
 def dose_driver():
-    dose_interface()
-    dose_input()
+    diagnosis = diagnosis_input()
+    weight = weight_input()
+    dosage = dose_amount(diagnosis, weight)
+    dose_output(weight, dosage)
 
-def dose_interface():
+def diagnosis_input():
     print("Day One Dosing Guidelines")
     print("")
     print("Choose diagnosis:")
@@ -18,9 +20,10 @@ def dose_interface():
     print("2 - Acute bacterial sinusitis")
     print("3 - Community-acquired pneumonia")
     print("4 - Pharyngitis/tonsilitis")
-
-def dose_input():
     diagnosis = int(input("Enter a number: "))
+    return diagnosis
+
+def weight_input():
     print("PATIENT WEIGHT")
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
@@ -30,18 +33,19 @@ def dose_input():
     units = weight_data[1]
     if units == "lb":
         weight = weight / 2.205
-    return weight_data
+    return weight
 
-
-def dose_amount():
-
+def dose_amount(diagnosis, weight):
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
+    return dosage_mg_first_day
+
+def dose_output(weight, dosage):
     print("CORRECT DOSAGE")
     print("For a patient weighing {:.1f} kg,".format(weight))
     print("  the correct dosage is {:.1f} mg the first day"
-          .format(dosage_mg_first_day))
+          .format(dosage))
 
 
 if __name__ == '__main__':
