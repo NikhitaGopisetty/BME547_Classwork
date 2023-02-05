2f4dccdb7399c45eaf516e8f5da5ea228cb6f201
NikhitaGopisetty <nikhita.gopisetty@duke.edu>
2023-02-05 14:41:22 -0500

    edited the coordinates function to separate the user inputs into its own function and added parameters to the coordinates_function in the file and the test file

--- coordinates.py
+++ coordinates.py
@@ -1,13 +1,18 @@
 # coordinates.py
 # Nikhita Gopisetty
 
-def coordinates_function():
+def input_function():
     x1, y1 = map(float, input("Enter the first coordinate (values separated by one space) 'x y': ").split())
     x2, y2 = map(float, input("Enter the first coordinate (values separated by one space) 'x y': ").split())
     x = float(input("Enter a new value on the x-coordinate of the above plane: "))
+    return x1, y1, x2, y2, x
+    
+def coordinates_function(x1, y1, x2, y2, x):
     m = (y2 - y1) / (x2 - x1)
     b = y1 - m * x1
     answer = m * x + b
     return answer
 
-coordinates_function()
+if __name__ == '__main__':
+    x1, y1, x2, y2, x = input_function()
+    coordinates_function(x1, y1, x2, y2, x)
