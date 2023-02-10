48ec8f8125f8d7b5523f3db8290e15ebe0974759
NikhitaGopisetty <nikhita.gopisetty@duke.edu>
2023-02-10 17:54:01 -0500

    Made PEP-8 changes to database.py and database_class.py

--- database_class.py
+++ database_class.py
@@ -1,5 +1,5 @@
 class Patient:
-    
+
     def __init__(self, patient_first_name,
                  patient_last_name,
                  patient_mrn,
@@ -9,12 +9,12 @@ class Patient:
         self.mrn = patient_mrn
         self.age = patient_age
         self.tests = []
-        
+
     def get_full_name(self):
         full_name = "{} {}".format(self.first_name,
                                    self.last_name)
         return full_name
-    
+
     def is_adult(self):
         if self.age >= 18:
             return True
