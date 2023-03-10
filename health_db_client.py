6302f086f06733d215063fe69db2ac05c5bfad15
NikhitaGopisetty <nikhita.gopisetty@duke.edu>
2023-03-10 12:53:36 -0500

    Additional work on March 10 in class

--- health_db_client.py
+++ health_db_client.py
@@ -2,22 +2,21 @@ import requests
 
 server = "http://127.0.0.1:5000"
 
-patient = {"id": 1, "name": "Nikhita", "blood_type": "O+"}
+patient = {"id": 1, "name": "David", "blood_type": "O+"}
 r = requests.post(server + "/new_patient", json=patient)
 print(r.status_code)
 print(r.text)
 
-patient = {"id": 2, "name": "John", "blood_type": "A+"}
+patient = {"mrn": 2, "name": "John", "blood_type": "A+"}
 r = requests.post(server + "/new_patient", json=patient)
 print(r.status_code)
 print(r.text)
 
-test = {"id": 1, "test_name": "HDL", "test_result": 150}
-r = requests.post(server + "/add_test", json=test)
+data = {"id": 1, "test_name": "HDL", "test_result": 150}
+r = requests.post(server + "/add_test", json=data)
 print(r.status_code)
 print(r.text)
 
-test = {"id": 1, "test_name": "LDL", "test_result": 100}
-r = requests.post(server + "/add_test", json=test)
+r = requests.get(server + "/get_results/1")
 print(r.status_code)
 print(r.text)
