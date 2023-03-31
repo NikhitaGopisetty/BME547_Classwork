138e4be44cedec46e4399a7b5234bcc00fdb32cf
NikhitaGopisetty <nikhita.gopisetty@duke.edu>
2023-03-31 12:44:10 -0400

    Finished GUI for database

--- db_gui.py
+++ db_gui.py
@@ -1,5 +1,36 @@
 import tkinter as tk
 from tkinter import ttk
+import requests
+
+
+def create_blood_string(blood_letter, rh):
+    blood_string = "{}{}".format(blood_letter, rh)
+    return blood_string
+
+
+def id_number_verification(id_number):
+    if 1 <= id_number <= 1000000:
+        return True
+    else:
+        return False
+
+
+def send_data_to_server(patient_name, id_number, blood_string,):
+    patient = {"id": id_number,
+               "name": patient_name,
+               "blood_type": blood_string}
+    r = requests.post("http://127.0.0.1:5000/new_patient", json=patient)
+    return r.text
+
+
+def check_and_upload_data(patient_name, id_number, blood_letter, rh,
+                          donation_center):
+    id_number = int(id_number)
+    blood_string = create_blood_string(blood_letter, rh)
+    if id_number_verification(id_number) is False:
+        return "ID number is incorrect"
+    msg = send_data_to_server(patient_name, id_number, blood_string)
+    return msg
 
 
 def set_up_window():
@@ -10,32 +41,33 @@ def set_up_window():
         patient_id = id_value.get()
         blood_letter = blood_letter_value.get()
         rh = rh_factor_value.get()
-        print("Patient name is {}".format(patient_name))
-        print("Patient ID is {}".format(patient_id))
-        print("Patient blood type is {}{}".format(blood_letter, rh))
+        donation_center = donation_value.get()
+        msg = check_and_upload_data(patient_name, patient_id, blood_letter, rh,
+                                    donation_center)
+        status_label.configure(text=msg)
 
     def cancel_btn_cmd():
         root.destroy()
 
     root = tk.Tk()
     root.title("Donor Database GUI")
-    root.geometry("800x400")
+    # root.geometry("800x400")
 
     top_label = ttk.Label(root, text="Blood Donor Database")
-    top_label.grid(column=0, row=0)
+    top_label.grid(column=0, row=0, columnspan=2, sticky=tk.W)
 
     name_label = ttk.Label(root, text="Name:")
-    name_label.grid(column=0, row=1)
+    name_label.grid(column=0, row=1, sticky=tk.E)
     name_value = tk.StringVar()
     # name_value.set("Enter your name here")
     name_entry = ttk.Entry(root, textvariable=name_value)
-    name_entry.grid(column=1, row=1)
+    name_entry.grid(column=1, row=1, padx=5)
 
     id_label = ttk.Label(root, text="ID:")
-    id_label.grid(column=0, row=2)
+    id_label.grid(column=0, row=2, sticky=tk.E)
     id_value = tk.StringVar()
     id_entry = ttk.Entry(root, textvariable=id_value)
-    id_entry.grid(column=1, row=2)
+    id_entry.grid(column=1, row=2, padx=5)
 
     ok_button = ttk.Button(root, text="OK", command=ok_btn_cmd)
     ok_button.grid(column=1, row=6)
@@ -47,19 +79,19 @@ def set_up_window():
 
     A_check = ttk.Radiobutton(root, text="A", variable=blood_letter_value,
                               value="A")
-    A_check.grid(column=0, row=3)
+    A_check.grid(column=0, row=3, sticky=tk.W)
 
     B_check = ttk.Radiobutton(root, text="B", variable=blood_letter_value,
                               value="B")
-    B_check.grid(column=0, row=4)
+    B_check.grid(column=0, row=4, sticky=tk.W)
 
     AB_check = ttk.Radiobutton(root, text="AB", variable=blood_letter_value,
                                value="AB")
-    AB_check.grid(column=0, row=5)
+    AB_check.grid(column=0, row=5, sticky=tk.W)
 
     O_check = ttk.Radiobutton(root, text="O", variable=blood_letter_value,
                               value="O")
-    O_check.grid(column=0, row=6)
+    O_check.grid(column=0, row=6, sticky=tk.W)
 
     rh_factor_value = tk.StringVar()
     rh_factor_value.set("-")
@@ -67,6 +99,17 @@ def set_up_window():
                                        variable=rh_factor_value,
                                        onvalue='+', offvalue='-')
     check_box_widget.grid(column=1, row=4)
+
+    donation_label = ttk.Label(root, text="Closest Donation Center")
+    donation_label.grid(column=2, row=0)
+    donation_value = tk.StringVar()
+    donation_combobox = ttk.Combobox(root, textvariable=donation_value)
+    donation_combobox.grid(column=2, row=1)
+    donation_combobox["values"] = ("Durham", "Apex", "Raleigh")
+    donation_combobox.state(["readonly"])
+
+    status_label = ttk.Label(root, text="")
+    status_label.grid(row=7, column=0, columnspan=10)
 
     root.mainloop()
 
